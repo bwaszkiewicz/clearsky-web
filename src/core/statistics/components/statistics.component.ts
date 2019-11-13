7a04b0d848473e9b3b0d89925a5e393ce905573b
bwaszkiewicz <be.waszkiewicz@gmail.com>
2019-11-13 18:15:28 +0100

    Add interval for AQI calculate

--- src/core/statistics/components/statistics.component.ts
+++ src/core/statistics/components/statistics.component.ts
@@ -28,6 +28,7 @@ export class StatisticsComponent implements OnInit {
     this.checkAqi();
 
     setInterval(() => {this.getLastUpdate(); }, 5000);
+    setInterval(() => {this.checkAqi(); }, 5 * 60 * 1000);
   }
 
   getLastUpdate() {
